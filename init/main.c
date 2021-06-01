dfa1752b134d4879617bdc2e1ed10fb3ac269af0
xueshi hu <hubachelar@gmail.com>
2021-06-01 15:47:16 +0800

    (#6) unable to trigger mmio

--- init/main.c
+++ init/main.c
@@ -4,13 +4,12 @@ unsigned long fw_arg0, fw_arg1, fw_arg2, fw_arg3;
 
 unsigned char kernel_stack[_THREAD_SIZE];
 
+void prom_putchar(char c);
 
-void start_kernel(){
-  // TODO init console
-
-  char a[] = "yyyyyyyyyyyyyyyyy";
-  while(1){
-
+void start_kernel() {
+  while (1) {
+    *(char *)(0x1fe001e0) = 'a';
   }
+
   // TODO extract acpi
 }
