fe5b1d67ca1de5ce486a2be6c0a06145be320775
xueshi hu <hubachelar@gmail.com>
2021-07-20 16:57:37 +0800

    (FIXME 112): add xxhash.h

--- include/exec/tb-hash.h
+++ include/exec/tb-hash.h
@@ -3,6 +3,7 @@
 
 #include "../exec/cpu-defs.h"
 #include "../exec/exec-all.h"
+#include "../qemu/xxhash.h"
 
 #ifdef CONFIG_SOFTMMU
 
@@ -14,39 +15,32 @@
 #define TB_JMP_ADDR_MASK (TB_JMP_PAGE_SIZE - 1)
 #define TB_JMP_PAGE_MASK (TB_JMP_CACHE_SIZE - TB_JMP_PAGE_SIZE)
 
-static inline unsigned int tb_jmp_cache_hash_page(target_ulong pc)
-{
-    target_ulong tmp;
-    tmp = pc ^ (pc >> (TARGET_PAGE_BITS - TB_JMP_PAGE_BITS));
-    return (tmp >> (TARGET_PAGE_BITS - TB_JMP_PAGE_BITS)) & TB_JMP_PAGE_MASK;
+static inline unsigned int tb_jmp_cache_hash_page(target_ulong pc) {
+  target_ulong tmp;
+  tmp = pc ^ (pc >> (TARGET_PAGE_BITS - TB_JMP_PAGE_BITS));
+  return (tmp >> (TARGET_PAGE_BITS - TB_JMP_PAGE_BITS)) & TB_JMP_PAGE_MASK;
 }
 
-static inline unsigned int tb_jmp_cache_hash_func(target_ulong pc)
-{
-    target_ulong tmp;
-    tmp = pc ^ (pc >> (TARGET_PAGE_BITS - TB_JMP_PAGE_BITS));
-    return (((tmp >> (TARGET_PAGE_BITS - TB_JMP_PAGE_BITS)) & TB_JMP_PAGE_MASK)
-           | (tmp & TB_JMP_ADDR_MASK));
+static inline unsigned int tb_jmp_cache_hash_func(target_ulong pc) {
+  target_ulong tmp;
+  tmp = pc ^ (pc >> (TARGET_PAGE_BITS - TB_JMP_PAGE_BITS));
+  return (((tmp >> (TARGET_PAGE_BITS - TB_JMP_PAGE_BITS)) & TB_JMP_PAGE_MASK) |
+          (tmp & TB_JMP_ADDR_MASK));
 }
 
 #else
 
 /* In user-mode we can get better hashing because we do not have a TLB */
-static inline unsigned int tb_jmp_cache_hash_func(target_ulong pc)
-{
-    return (pc ^ (pc >> TB_JMP_CACHE_BITS)) & (TB_JMP_CACHE_SIZE - 1);
+static inline unsigned int tb_jmp_cache_hash_func(target_ulong pc) {
+  return (pc ^ (pc >> TB_JMP_CACHE_BITS)) & (TB_JMP_CACHE_SIZE - 1);
 }
 
 #endif /* CONFIG_SOFTMMU */
 
-// FIXME a little function, copy it later
-uint32_t qemu_xxhash7(uint64_t ab, uint64_t cd, uint32_t e, uint32_t f, uint32_t g);
-
-static inline
-uint32_t tb_hash_func(tb_page_addr_t phys_pc, target_ulong pc, uint32_t flags,
-                      uint32_t cf_mask, uint32_t trace_vcpu_dstate)
-{
-    return qemu_xxhash7(phys_pc, pc, flags, cf_mask, trace_vcpu_dstate);
+static inline uint32_t tb_hash_func(tb_page_addr_t phys_pc, target_ulong pc,
+                                    uint32_t flags, uint32_t cf_mask,
+                                    uint32_t trace_vcpu_dstate) {
+  return qemu_xxhash7(phys_pc, pc, flags, cf_mask, trace_vcpu_dstate);
 }
 
 #endif /* end of include guard: TB_HASH_H_HPADT4RC */
