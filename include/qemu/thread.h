aa30a914c6ca1378a8e30bfd37e392971ac503b4
xueshi hu <hubachelar@gmail.com>
2021-07-26 20:23:12 +0800

    (code review): remove thread-posix.h, it's a really simple file

--- include/qemu/thread.h
+++ include/qemu/thread.h
@@ -2,11 +2,14 @@
 #define THREAD_H_P9X05SBE
 
 #include <stdbool.h>
-#include "thread-posix.h"
 
 typedef struct QemuSpin {
   int value;
 } QemuSpin;
+
+typedef struct QemuMutex {
+  // FIXME
+} QemuMutex;
 
 static inline void qemu_spin_init(QemuSpin *spin) {}
 
@@ -14,8 +17,8 @@ static inline void qemu_spin_lock(QemuSpin *spin) {}
 
 static inline void qemu_spin_unlock(QemuSpin *spin) {}
 
-static inline bool qemu_spin_trylock(QemuSpin *spin) {return true;}
+static inline bool qemu_spin_trylock(QemuSpin *spin) { return true; }
 
-void qemu_mutex_init(QemuMutex *mutex){}
+void qemu_mutex_init(QemuMutex *mutex) {}
 
 #endif /* end of include guard: THREAD_H_P9X05SBE */
