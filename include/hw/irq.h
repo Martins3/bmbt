813f0ff6ca232ecb0dd4becb91fd1108970d39b5
xueshi hu <hubachelar@gmail.com>
2021-09-19 22:10:06 +0800

    :bird: port i8259

--- include/hw/irq.h
+++ include/hw/irq.h
@@ -3,12 +3,6 @@
 
 typedef void (*qemu_irq_handler)(void *opaque, int n, int level);
 
-struct IRQState {
-  qemu_irq_handler handler;
-  void *opaque;
-  int n;
-};
-
 /*
  * Pointer types
  * Such typedefs should be limited to cases where the typedef's users
@@ -17,13 +11,10 @@ struct IRQState {
  */
 typedef struct IRQState *qemu_irq;
 
-void qemu_set_irq(qemu_irq irq, int level) {
-  if (!irq)
-    return;
-
-  irq->handler(irq->opaque, irq->n, level);
-}
+void qemu_set_irq(qemu_irq irq, int level);
 
 static inline void qemu_irq_raise(qemu_irq irq) { qemu_set_irq(irq, 1); }
+
+static inline void qemu_irq_lower(qemu_irq irq) { qemu_set_irq(irq, 0); }
 
 #endif /* end of include guard: IRQ_H_TZGVAY9K */
---
+++ include/hw/isa/i8259_internal.h
@@ -0,0 +1,34 @@
+#include "../../types.h"
+#include "../irq.h"
+typedef struct {
+  // ISADevice parent_obj;
+
+  uint8_t last_irr;     /* edge detection */
+  uint8_t irr;          /* interrupt request register */
+  uint8_t imr;          /* interrupt mask register */
+  uint8_t isr;          /* interrupt service register */
+  uint8_t priority_add; /* highest irq priority */
+  uint8_t irq_base;
+  uint8_t read_reg_select;
+  uint8_t poll;
+  uint8_t special_mask;
+  uint8_t init_state;
+  uint8_t auto_eoi;
+  uint8_t rotate_on_auto_eoi;
+  uint8_t special_fully_nested_mode;
+  uint8_t init4;       /* true if 4 byte init */
+  uint8_t single_mode; /* true if slave pic is not initialized */
+  uint8_t elcr;        /* PIIX edge/trigger selection*/
+  uint8_t elcr_mask;
+  qemu_irq int_out[1];
+  uint32_t master; /* reflects /SP input pin */
+  uint32_t iobase;
+  uint32_t elcr_addr;
+  // @todo fix it in memory model module
+  // MemoryRegion base_io;
+  // MemoryRegion elcr_io;
+} PICCommonState;
+
+void pic_reset_common(PICCommonState *s);
+void *i8259_init_chip(const char *name, bool master);
+void pic_stat_update_irq(int irq, int level);
