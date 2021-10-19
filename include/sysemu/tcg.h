10a44cee5c2b1253c15acb281b34d2c94de4fb15
xueshi hu <hubachelar@gmail.com>
2021-10-19 22:03:32 +0800

    fix: get tcg_allowed initialized earlier

--- include/sysemu/tcg.h
+++ include/sysemu/tcg.h
@@ -1,13 +1,18 @@
 #ifndef TCG_H_ZDBR7L4E
 #define TCG_H_ZDBR7L4E
 
+#include "../qemu/compiler.h"
 #include "../qemu/config-host.h"
+#include <assert.h>
 #include <stdbool.h>
 
 void tcg_exec_init(unsigned long tb_size);
 int tcg_init();
 
 extern bool tcg_allowed;
-#define tcg_enabled() (tcg_allowed)
+static inline bool tcg_enabled() {
+  duck_check(!tcg_allowed);
+  return tcg_allowed;
+}
 
 #endif /* end of include guard: TCG_H_ZDBR7L4E */
