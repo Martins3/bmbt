b9a943d4aa3b10c69fc60f38a5e6111110a9b5b7
xueshi hu <hubachelar@gmail.com>
2021-10-21 15:47:58 +0800

    refactor(memory model): port memory_region_dispatch_read/write(5/6)

--- src/qemu/memory.c
+++ src/qemu/memory.c
@@ -155,6 +155,474 @@ MemoryRegion *address_space_translate(AddressSpace *as, hwaddr addr,
                                       MemTxAttrs attrs) {
   return as->mr_look_up(as, addr, xlat, len);
 }
+
+static inline MemoryRegion *flatview_translate(AddressSpace *as, hwaddr addr,
+                                               hwaddr *xlat, hwaddr *len,
+                                               bool is_write,
+                                               MemTxAttrs attrs) {
+  return address_space_translate(as, addr, xlat, len, is_write, attrs);
+}
+
+static inline void memory_region_shift_read_access(uint64_t *value,
+                                                   signed shift, uint64_t mask,
+                                                   uint64_t tmp) {
+  if (shift >= 0) {
+    *value |= (tmp & mask) << shift;
+  } else {
+    *value |= (tmp & mask) >> -shift;
+  }
+}
+
+static inline uint64_t
+memory_region_shift_write_access(uint64_t *value, signed shift, uint64_t mask) {
+  uint64_t tmp;
+
+  if (shift >= 0) {
+    tmp = (*value >> shift) & mask;
+  } else {
+    tmp = (*value << -shift) & mask;
+  }
+
+  return tmp;
+}
+
+static MemTxResult memory_region_read_accessor(MemoryRegion *mr, hwaddr addr,
+                                               uint64_t *value, unsigned size,
+                                               signed shift, uint64_t mask,
+                                               MemTxAttrs attrs) {
+  uint64_t tmp;
+
+  tmp = mr->ops->read(mr->opaque, addr, size);
+#ifdef BMBT
+  if (mr->subpage) {
+    trace_memory_region_subpage_read(get_cpu_index(), mr, addr, tmp, size);
+  } else if (TRACE_MEMORY_REGION_OPS_READ_ENABLED) {
+    hwaddr abs_addr = memory_region_to_absolute_addr(mr, addr);
+    trace_memory_region_ops_read(get_cpu_index(), mr, abs_addr, tmp, size);
+  }
+#endif
+  memory_region_shift_read_access(value, shift, mask, tmp);
+  return MEMTX_OK;
+}
+
+static MemTxResult memory_region_write_accessor(MemoryRegion *mr, hwaddr addr,
+                                                uint64_t *value, unsigned size,
+                                                signed shift, uint64_t mask,
+                                                MemTxAttrs attrs) {
+  uint64_t tmp = memory_region_shift_write_access(value, shift, mask);
+
+#ifdef BMBT
+  if (mr->subpage) {
+    trace_memory_region_subpage_write(get_cpu_index(), mr, addr, tmp, size);
+  } else if (TRACE_MEMORY_REGION_OPS_WRITE_ENABLED) {
+    hwaddr abs_addr = memory_region_to_absolute_addr(mr, addr);
+    trace_memory_region_ops_write(get_cpu_index(), mr, abs_addr, tmp, size);
+  }
+#endif
+  mr->ops->write(mr->opaque, addr, tmp, size);
+  return MEMTX_OK;
+}
+
+static bool memory_region_big_endian(MemoryRegion *mr) {
+#ifdef TARGET_WORDS_BIGENDIAN
+  return mr->ops->endianness != DEVICE_LITTLE_ENDIAN;
+#else
+  return mr->ops->endianness == DEVICE_BIG_ENDIAN;
+#endif
+}
+
+static MemTxResult access_with_adjusted_size(
+    hwaddr addr, uint64_t *value, unsigned size, unsigned access_size_min,
+    unsigned access_size_max,
+    MemTxResult (*access_fn)(MemoryRegion *mr, hwaddr addr, uint64_t *value,
+                             unsigned size, signed shift, uint64_t mask,
+                             MemTxAttrs attrs),
+    MemoryRegion *mr, MemTxAttrs attrs) {
+  uint64_t access_mask;
+  unsigned access_size;
+  unsigned i;
+  MemTxResult r = MEMTX_OK;
+
+  if (!access_size_min) {
+    access_size_min = 1;
+  }
+  if (!access_size_max) {
+    access_size_max = 4;
+  }
+
+  /* FIXME: support unaligned access? */
+  access_size = MAX(MIN(size, access_size_max), access_size_min);
+  access_mask = MAKE_64BIT_MASK(0, access_size * 8);
+  if (memory_region_big_endian(mr)) {
+    for (i = 0; i < size; i += access_size) {
+      r |= access_fn(mr, addr + i, value, access_size,
+                     (size - access_size - i) * 8, access_mask, attrs);
+    }
+  } else {
+    for (i = 0; i < size; i += access_size) {
+      r |= access_fn(mr, addr + i, value, access_size, i * 8, access_mask,
+                     attrs);
+    }
+  }
+  return r;
+}
+
+static MemTxResult memory_region_dispatch_read1(MemoryRegion *mr, hwaddr addr,
+                                                uint64_t *pval, unsigned size,
+                                                MemTxAttrs attrs) {
+  *pval = 0;
+
+  if (mr->ops->read) {
+    return access_with_adjusted_size(
+        addr, pval, size, mr->ops->impl.min_access_size,
+        mr->ops->impl.max_access_size, memory_region_read_accessor, mr, attrs);
+  }
+  //
+  g_assert_not_reached();
+}
+
+static void adjust_endianness(MemoryRegion *mr, uint64_t *data, MemOp op) {
+  if ((op & MO_BSWAP) != devend_memop(mr->ops->endianness)) {
+    switch (op & MO_SIZE) {
+    case MO_8:
+      break;
+    case MO_16:
+      *data = bswap16(*data);
+      break;
+    case MO_32:
+      *data = bswap32(*data);
+      break;
+    case MO_64:
+      *data = bswap64(*data);
+      break;
+    default:
+      g_assert_not_reached();
+    }
+  }
+}
+
+bool memory_region_access_valid(MemoryRegion *mr, hwaddr addr, unsigned size,
+                                bool is_write, MemTxAttrs attrs) {
+  int access_size_min, access_size_max;
+  int access_size, i;
+
+  if (!mr->ops->valid.unaligned && (addr & (size - 1))) {
+    return false;
+  }
+
+  if (!mr->ops->valid.accepts) {
+    return true;
+  }
+
+  access_size_min = mr->ops->valid.min_access_size;
+  if (!mr->ops->valid.min_access_size) {
+    access_size_min = 1;
+  }
+
+  access_size_max = mr->ops->valid.max_access_size;
+  if (!mr->ops->valid.max_access_size) {
+    access_size_max = 4;
+  }
+
+  access_size = MAX(MIN(size, access_size_max), access_size_min);
+  for (i = 0; i < size; i += access_size) {
+    // @todo mem review code here
+    // why we need accepts
+    if (!mr->ops->valid.accepts(mr->opaque, addr + i, access_size, is_write,
+                                attrs)) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+MemTxResult memory_region_dispatch_read(MemoryRegion *mr, hwaddr addr,
+                                        uint64_t *pval, MemOp op,
+                                        MemTxAttrs attrs) {
+  unsigned size = memop_size(op);
+  MemTxResult r;
+
+  if (!memory_region_access_valid(mr, addr, size, false, attrs)) {
+    g_assert_not_reached();
+    // *pval = unassigned_mem_read(mr, addr, size);
+    return MEMTX_DECODE_ERROR;
+  }
+
+  r = memory_region_dispatch_read1(mr, addr, pval, size, attrs);
+  adjust_endianness(mr, pval, op);
+  return r;
+}
+
+MemTxResult memory_region_dispatch_write(MemoryRegion *mr, hwaddr addr,
+                                         uint64_t data, MemOp op,
+                                         MemTxAttrs attrs) {
+  unsigned size = memop_size(op);
+
+  if (!memory_region_access_valid(mr, addr, size, true, attrs)) {
+    g_assert_not_reached();
+    // unassigned_mem_write(mr, addr, data, size);
+    return MEMTX_DECODE_ERROR;
+  }
+
+  adjust_endianness(mr, &data, op);
+
+  // no virtio eventfd notifier
+#ifdef BMBT
+  if ((!kvm_eventfds_enabled()) &&
+      memory_region_dispatch_write_eventfds(mr, addr, data, size, attrs)) {
+    return MEMTX_OK;
+  }
+#endif
+
+  if (mr->ops->write) {
+    return access_with_adjusted_size(
+        addr, &data, size, mr->ops->impl.min_access_size,
+        mr->ops->impl.max_access_size, memory_region_write_accessor, mr, attrs);
+  }
+  g_assert_not_reached();
+}
+
+static int memory_access_size(MemoryRegion *mr, unsigned l, hwaddr addr) {
+  unsigned access_size_max = mr->ops->valid.max_access_size;
+
+  /* Regions are assumed to support 1-4 byte accesses unless
+     otherwise specified.  */
+  if (access_size_max == 0) {
+    access_size_max = 4;
+  }
+
+  /* Bound the maximum access by the alignment of the address.  */
+  if (!mr->ops->impl.unaligned) {
+    unsigned align_size_max = addr & -addr;
+    if (align_size_max != 0 && align_size_max < access_size_max) {
+      access_size_max = align_size_max;
+    }
+  }
+
+  /* Don't attempt accesses larger than the maximum.  */
+  if (l > access_size_max) {
+    l = access_size_max;
+  }
+  l = pow2floor(l);
+
+  return l;
+}
+
+/* Return a host pointer to guest's ram. Similar to qemu_map_ram_ptr
+ * but takes a size argument.
+ *
+ * Called within RCU critical section.
+ */
+static void *qemu_ram_ptr_length(RAMBlock *ram_block, ram_addr_t addr,
+                                 hwaddr *size, bool lock) {
+  RAMBlock *block = ram_block;
+#ifdef BMBT
+  if (*size == 0) {
+    return NULL;
+  }
+
+  if (block == NULL) {
+    block = qemu_get_ram_block(addr);
+    addr -= block->offset;
+  }
+#endif
+  duck_check(block != NULL);
+  *size = MIN(*size, block->max_length - addr);
+
+#ifdef BMBT
+  if (xen_enabled() && block->host == NULL) {
+    /* We need to check if the requested address is in the RAM
+     * because we don't want to map the entire memory in QEMU.
+     * In that case just map the requested area.
+     */
+    if (block->offset == 0) {
+      return xen_map_cache(addr, *size, lock, lock);
+    }
+
+    block->host = xen_map_cache(block->offset, block->max_length, 1, lock);
+  }
+#endif
+
+  return ramblock_ptr(block, addr);
+}
+
+/* Called within RCU critical section.  */
+static MemTxResult flatview_write_continue(AddressSpace *fv, hwaddr addr,
+                                           MemTxAttrs attrs, const uint8_t *buf,
+                                           hwaddr len, hwaddr addr1, hwaddr l,
+                                           MemoryRegion *mr) {
+  uint8_t *ptr;
+  uint64_t val;
+  MemTxResult result = MEMTX_OK;
+  bool release_lock = false;
+
+  for (;;) {
+    if (!memory_access_is_direct(mr, true)) {
+      release_lock |= prepare_mmio_access(mr);
+      l = memory_access_size(mr, l, addr1);
+      /* XXX: could force current_cpu to NULL to avoid
+         potential bugs */
+      val = ldn_he_p(buf, l);
+      result |=
+          memory_region_dispatch_write(mr, addr1, val, size_memop(l), attrs);
+    } else {
+      /* RAM case */
+      ptr = qemu_ram_ptr_length(mr->ram_block, addr1, &l, false);
+      memcpy(ptr, buf, l);
+      invalidate_and_set_dirty(mr, addr1, l);
+    }
+
+    if (release_lock) {
+      qemu_mutex_unlock_iothread();
+      release_lock = false;
+    }
+
+    len -= l;
+    buf += l;
+    addr += l;
+
+    if (!len) {
+      break;
+    }
+
+    l = len;
+    mr = flatview_translate(fv, addr, &addr1, &l, true, attrs);
+  }
+
+  return result;
+}
+
+/* Called from RCU critical section.  */
+static MemTxResult flatview_write(AddressSpace *fv, hwaddr addr,
+                                  MemTxAttrs attrs, const uint8_t *buf,
+                                  hwaddr len) {
+  hwaddr l;
+  hwaddr addr1;
+  MemoryRegion *mr;
+  MemTxResult result = MEMTX_OK;
+
+  l = len;
+  mr = flatview_translate(fv, addr, &addr1, &l, true, attrs);
+  result = flatview_write_continue(fv, addr, attrs, buf, len, addr1, l, mr);
+
+  return result;
+}
+
+/* Called within RCU critical section.  */
+MemTxResult flatview_read_continue(AddressSpace *fv, hwaddr addr,
+                                   MemTxAttrs attrs, uint8_t *buf, hwaddr len,
+                                   hwaddr addr1, hwaddr l, MemoryRegion *mr) {
+  uint8_t *ptr;
+  uint64_t val;
+  MemTxResult result = MEMTX_OK;
+  bool release_lock = false;
+
+  for (;;) {
+    if (!memory_access_is_direct(mr, false)) {
+      /* I/O case */
+      release_lock |= prepare_mmio_access(mr);
+      l = memory_access_size(mr, l, addr1);
+      result |=
+          memory_region_dispatch_read(mr, addr1, &val, size_memop(l), attrs);
+      stn_he_p(buf, l, val);
+    } else {
+      /* RAM case */
+      ptr = qemu_ram_ptr_length(mr->ram_block, addr1, &l, false);
+      memcpy(buf, ptr, l);
+    }
+
+    if (release_lock) {
+      qemu_mutex_unlock_iothread();
+      release_lock = false;
+    }
+
+    len -= l;
+    buf += l;
+    addr += l;
+
+    if (!len) {
+      break;
+    }
+
+    l = len;
+    mr = flatview_translate(fv, addr, &addr1, &l, false, attrs);
+  }
+
+  return result;
+}
+
+/* Called from RCU critical section.  */
+static MemTxResult flatview_read(AddressSpace *fv, hwaddr addr,
+                                 MemTxAttrs attrs, uint8_t *buf, hwaddr len) {
+  hwaddr l;
+  hwaddr addr1;
+  MemoryRegion *mr;
+
+  l = len;
+  mr = flatview_translate(fv, addr, &addr1, &l, false, attrs);
+  return flatview_read_continue(fv, addr, attrs, buf, len, addr1, l, mr);
+}
+
+MemTxResult address_space_read_full(AddressSpace *fv, hwaddr addr,
+                                    MemTxAttrs attrs, uint8_t *buf,
+                                    hwaddr len) {
+  MemTxResult result = MEMTX_OK;
+
+  if (len > 0) {
+    RCU_READ_LOCK_GUARD();
+    result = flatview_read(fv, addr, attrs, buf, len);
+  }
+
+  return result;
+}
+
+MemTxResult address_space_write(AddressSpace *fv, hwaddr addr, MemTxAttrs attrs,
+                                const uint8_t *buf, hwaddr len) {
+  MemTxResult result = MEMTX_OK;
+
+  if (len > 0) {
+    RCU_READ_LOCK_GUARD();
+    result = flatview_write(fv, addr, attrs, buf, len);
+  }
+
+  return result;
+}
+
+MemTxResult address_space_rw(AddressSpace *as, hwaddr addr, MemTxAttrs attrs,
+                             uint8_t *buf, hwaddr len, bool is_write) {
+
+  if (is_write) {
+    return address_space_write(as, addr, attrs, buf, len);
+  } else {
+    return address_space_read_full(as, addr, attrs, buf, len);
+  }
+}
+
+bool prepare_mmio_access(MemoryRegion *mr) {
+  bool unlocked = !qemu_mutex_iothread_locked();
+  bool release_lock = false;
+
+  if (unlocked) {
+    qemu_mutex_lock_iothread();
+    unlocked = false;
+    release_lock = true;
+  }
+#ifdef BMBT
+  if (mr->flush_coalesced_mmio) {
+    if (unlocked) {
+      qemu_mutex_lock_iothread();
+    }
+    qemu_flush_coalesced_mmio_buffer();
+    if (unlocked) {
+      qemu_mutex_unlock_iothread();
+    }
+  }
+#endif
+
+  return release_lock;
+}
+
 // originally defined in dirty_memory_extend
 static void setup_dirty_memory(hwaddr total_ram_size) {
   ram_addr_t new_num_blocks =
