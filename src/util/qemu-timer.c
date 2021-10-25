bca09fc6f1191cfda2fe4e5c82355db42438ccb6
xueshi hu <hubachelar@gmail.com>
2021-10-25 12:38:18 +0800

    fix: add timer stub

---
+++ src/util/qemu-timer.c
@@ -0,0 +1,19 @@
+#include "../../include/qemu/timer.h"
+
+int64_t qemu_clock_get_ns(QEMUClockType type) {
+  // @todo
+  return 0;
+}
+
+void timer_mod(QEMUTimer *ts, int64_t expire_timer) {
+  // @todo
+}
+
+void timer_del(QEMUTimer *ts) {
+  // @todo
+}
+
+int64_t cpu_get_ticks(void) {
+  // @todo
+  return 0;
+}
