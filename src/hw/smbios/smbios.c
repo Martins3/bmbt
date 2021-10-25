ea104e53af0c05aed0e8113ae75a189e5f643adb
xueshi hu <hubachelar@gmail.com>
2021-10-25 13:18:30 +0800

    fix: add smbios and acpi stub

---
+++ src/hw/smbios/smbios.c
@@ -0,0 +1,6 @@
+#include "../../../include/hw/firmware/smbios.h"
+void smbios_set_defaults(const char *manufacturer, const char *product,
+                         const char *version, bool legacy_mode,
+                         bool uuid_encoded, SmbiosEntryPointType ep_type) {
+  // @todo
+}
