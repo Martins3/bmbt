315cb29e6373f17636e4c93be34333a286c3b217
xueshi hu <hubachelar@gmail.com>
2021-10-28 18:07:41 +0800

    test: address_space_rw

--- src/test/test-memory-model.c
+++ src/test/test-memory-model.c
@@ -1,10 +1,13 @@
+#include "../i386/cpu.h"
+#include "../i386/helper.h"
 #include <exec/memory.h>
 #include <exec/ram_addr.h>
 #include <qemu/units.h>
+#include <sysemu/cpus.h>
 #include <test/test.h>
 #include <unitest/greatest.h>
 
-TEST test_basic(void) {
+TEST test_env_setup(void) {
   memory_map_init(128 * MiB);
   RAMBlock *block;
   uint64_t end = 0;
@@ -33,14 +36,88 @@ TEST test_basic(void) {
   PASS();
 }
 
-TEST test_memory_ldst(void) { PASS(); }
+static uint64_t ops_read(void *opaque, hwaddr addr, unsigned size) {
+  MemoryRegion *mr = (MemoryRegion *)opaque;
+  return 0x12345678abcdef12;
+}
 
-TEST test_readonly_memory(void) { PASS(); }
+static void ops_write(void *opaque, hwaddr addr, uint64_t value,
+                      unsigned size) {
+  MemoryRegion *mr = (MemoryRegion *)opaque;
+  duck_check(value == 0x12345678);
+}
+
+static bool test_valid(void *opaque, hwaddr addr, unsigned size, bool is_write,
+                       MemTxAttrs attrs) {
+  return (size == 4) || (is_write && size == 8);
+}
+
+static const MemoryRegionOps ops1 = {
+    .read = ops_read,
+    .write = ops_write,
+    .endianness = DEVICE_LITTLE_ENDIAN,
+    .valid.accepts = test_valid,
+};
+
+TEST test_rw(void) {
+
+  // initialize
+  qemu_init_cpu_loop();
+  struct MemTxAttrs attrs;
+  CPUX86State env;
+  env.hflags = 0;
+  MemoryRegion *mr1 = g_malloc0(sizeof(MemoryRegion));
+  uint32_t port = 0x100;
+  memory_region_init_io(mr1, &ops1, mr1, "<ioport 1>", 4);
+  mr1->offset = port;
+  io_add_memory_region(port, mr1);
+
+  // helper
+  target_ulong x = helper_inl(&env, port);
+  ASSERT_EQ(x, 0xabcdef12);
+  helper_outl(&env, port, 0x12345678);
+
+  // address space io rw
+  uint64_t buf = 0x3f3f3f3f12345678;
+  MemTxResult mem =
+      address_space_rw(&address_space_io, port, attrs, (void *)&buf, 4, false);
+  ASSERT_EQ(buf, 0x3f3f3f3fabcdef12);
+  ASSERT_EQ(mem, MEMTX_OK);
+  buf = 0x3f3f3f3f12345678;
+  mem = address_space_rw(&address_space_io, port, attrs, (void *)&buf, 4, true);
+  ASSERT_EQ(mem, MEMTX_OK);
+
+  PASS();
+}
+
+TEST test_dirty_memory(void) {
+  struct MemTxAttrs attrs;
+  CPUX86State env;
+  MemTxResult mem;
+  env.hflags = 0;
+  hwaddr addr = 0x100000;
+  uint32_t msg_size = 0x100;
+  char *msg = (char *)g_malloc0(msg_size + 1);
+  char *msg2 = (char *)g_malloc0(msg_size + 1);
+  for (int i = 0; i < msg_size; ++i) {
+    msg[i] = 'x';
+  }
+
+  mem = address_space_rw(&address_space_memory, addr, attrs, (void *)msg,
+                         msg_size, true);
+  mem = address_space_rw(&address_space_io, addr, attrs, (void *)msg2, 8, true);
+  for (int i = 0; i < msg_size; ++i) {
+    ASSERT_EQ(msg[i], msg2[i]);
+  }
+  PASS();
+}
 
 TEST test_smc(void) { PASS(); }
 
 TEST test_pam(void) { PASS(); }
 
-TEST test_lookup(void) { PASS(); }
-
-SUITE(memory_model_test) { RUN_TEST(test_basic); }
+SUITE(memory_model_test) {
+  RUN_TEST(test_env_setup);
+  RUN_TEST(test_rw);
+  // RUN_TEST(test_dirty_memory);
+}
