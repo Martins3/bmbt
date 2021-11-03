dd7bd7300a51c515d664949d1147b711cbf89a3f
xueshi hu <hubachelar@gmail.com>
2021-11-03 17:24:25 +0800

    chore: fix overflow bug in snippet

--- script/snippt.c
+++ script/snippt.c
@@ -7,8 +7,13 @@ static void huxueshi(long irq) {
 
 static long database[100];
 static int counter = 0;
+static int is_full = 0;
 
 static int insert_counter(long data) {
+  if (is_full) {
+    printf("%s full\n", __FUNCTION__);
+    return 0;
+  }
   for (int i = 0; i < counter; ++i) {
     if (database[i] == data) {
       return 0;
@@ -16,5 +21,8 @@ static int insert_counter(long data) {
   }
   huxueshi(data);
   database[counter++] = data;
+  if (counter == 100) {
+    is_full = 1;
+  }
   return 1;
 }
