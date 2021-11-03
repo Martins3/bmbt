199317b6de91688e825607cd481ffe81130a5d24
xueshi hu <hubachelar@gmail.com>
2021-11-03 21:50:16 +0800

    fix: `CPU_NEXT` can return null

--- src/qemu/cpus.c
+++ src/qemu/cpus.c
@@ -209,7 +209,6 @@ void *qemu_tcg_rr_cpu_thread_fn(void *arg) {
 
     if (!cpu) {
       cpu = first_cpu;
-      g_assert_not_reached();
     }
 
     while (cpu && !cpu->queued_work_first && !cpu->exit_request) {
