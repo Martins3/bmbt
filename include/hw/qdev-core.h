cb641283ea882ea1b42dc7c222e305080acd9816
xueshi hu <hubachelar@gmail.com>
2021-11-21 13:11:54 +0800

    refactor: separate DeviceState definitions to qdev-core.h

---
+++ include/hw/qdev-core.h
@@ -0,0 +1,7 @@
+#ifndef QDEV_CORE_H_YZDAV31L
+#define QDEV_CORE_H_YZDAV31L
+
+// currently, DeviceState is empty
+typedef struct DeviceState DeviceState;
+
+#endif /* end of include guard: QDEV_CORE_H_YZDAV31L */
