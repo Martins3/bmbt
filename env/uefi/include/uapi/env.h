2a4b144d208d93e301feeb4d14dd9195fb20c3f4
xueshi hu <hubachelar@gmail.com>
2021-12-21 18:03:52 +0800

    fix: add sigjmp_buf definition

--- env/uefi/include/uapi/env.h
+++ env/uefi/include/uapi/env.h
@@ -1,4 +1,12 @@
 #ifndef ENV_H_RI3Y14ON
 #define ENV_H_RI3Y14ON
 
+#include <setjmp.h>
+/**
+ * jmp_buf and sigjmp_buf are actually the same thing
+ *
+ * see musl/include/setjmp.h
+ */
+typedef jmp_buf sigjmp_buf;
+
 #endif /* end of include guard: ENV_H_RI3Y14ON */
