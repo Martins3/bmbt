7522b808d5ae5f5de85cd0e359d2cea067732489
xueshi hu <hubachelar@gmail.com>
2021-12-22 11:22:50 +0800

    fix: exchange uefi and userspace's env.c

--- env/userspace/env.c
+++ env/userspace/env.c
@@ -1,10 +1,12 @@
 #include <assert.h>
 #include <exec/hwaddr.h>
-#include <stdlib.h>
+#include <sys/mman.h>
 #include <uapi/env.h>
 
 void *alloc_ram(hwaddr size) {
-  void *host = malloc(size);
-  assert(host != NULL);
+  // (qemu) qemu_ram_mmap size=0x180200000 flags=0x22 guardfd=-1
+  void *host = mmap(0, size, PROT_EXEC | PROT_READ | PROT_WRITE,
+                    MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+  assert(host != (void *)-1);
   return host;
 }
