3f9065f850bd0be83d31999d4e3b665dcfb0483d
xueshi hu <hubachelar@gmail.com>
2021-12-25 14:50:19 +0800

    fix: use uapi/libc.h to resolve redefined `NULL`
    NULL already defined in MdePkg，when compile bmbt in UEFI, with or
    without StdLib, we can't define NULL again in our ported libc

---
+++ env/uefi/include/uapi/libc.h
@@ -0,0 +1,4 @@
+#ifndef LIBC_H_CJ5RZTDU
+#define LIBC_H_CJ5RZTDU
+
+#endif /* end of include guard: LIBC_H_CJ5RZTDU */
---
+++ env/userspace/include/uapi/libc.h
@@ -0,0 +1,10 @@
+#ifndef LIBC_H_YEIDELXV
+#define LIBC_H_YEIDELXV
+
+#ifdef __cplusplus
+#define NULL 0L
+#else
+#define NULL ((void *)0)
+#endif
+
+#endif /* end of include guard: LIBC_H_YEIDELXV */
