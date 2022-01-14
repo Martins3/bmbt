01232e08371959e27f1197313649789cbf109817
xueshi hu <hubachelar@gmail.com>
2022-01-14 19:42:57 +0800

    feat: add madvise and mprotect stub

--- env/loongarch/include/kernel-type.h
+++ env/loongarch/include/kernel-type.h
@@ -1,9 +1,11 @@
 #ifndef KERNEL_TYPE_H_M4EKAC83
 #define KERNEL_TYPE_H_M4EKAC83
 
+#ifndef __ASSEMBLY__
 #include <inttypes.h>
 typedef uint64_t u64;
 typedef uint32_t u32;
 typedef uint16_t u16;
 typedef uint8_t u8;
+#endif
 #endif /* end of include guard: KERNEL_TYPE_H_M4EKAC83 */
