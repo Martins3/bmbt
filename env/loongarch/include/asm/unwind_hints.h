427c73d234b0dad1514aa27dc14fa7330b841846
xueshi hu <hubachelar@gmail.com>
2022-01-17 11:31:29 +0800

    fix: don't format a header containing asm macros

--- env/loongarch/include/asm/unwind_hints.h
+++ env/loongarch/include/asm/unwind_hints.h
@@ -23,34 +23,38 @@
  * the debuginfo as necessary.  It will also warn if it sees any
  * inconsistencies.
  */
-.macro UNWIND_HINT sp_reg = ORC_REG_SP sp_offset = 0 type = ORC_TYPE_CALL end =
-    0
+.macro UNWIND_HINT sp_reg=ORC_REG_SP sp_offset=0 type=ORC_TYPE_CALL end=0
 #ifdef CONFIG_STACK_VALIDATION
-        .Lunwind_hint_ip_\
-    @ :.pushsection.discard
-        .unwind_hints
-        /* struct unwind_hint */
-        .long.Lunwind_hint_ip_\
-       @-..short \sp_offset.byte \sp_reg.byte \type.byte \end.balign 4
-        .popsection
+.Lunwind_hint_ip_\@:
+	.pushsection .discard.unwind_hints
+		/* struct unwind_hint */
+		.long .Lunwind_hint_ip_\@ - .
+		.short \sp_offset
+		.byte \sp_reg
+		.byte \type
+		.byte \end
+		.balign 4
+	.popsection
 #endif
-        .endm
+.endm
 
-        .macro UNWIND_HINT_EMPTY UNWIND_HINT sp_reg = ORC_REG_UNDEFINED end =
-        1.endm
+.macro UNWIND_HINT_EMPTY
+	UNWIND_HINT sp_reg=ORC_REG_UNDEFINED end=1
+.endm
 
-            .macro UNWIND_HINT_REGS base = ORC_REG_SP offset =
-            0 UNWIND_HINT sp_reg =\base sp_offset =\offset type =
-                ORC_TYPE_REGS
-                    .endm
+.macro UNWIND_HINT_REGS base=ORC_REG_SP offset=0
+	UNWIND_HINT sp_reg=\base sp_offset=\offset type=ORC_TYPE_REGS
+.endm
 
-                    .macro UNWIND_HINT_FUNC offset = 0 UNWIND_HINT
-                    sp_offset =\offset
-                                   .endm
+.macro UNWIND_HINT_FUNC offset=0
+	UNWIND_HINT sp_offset=\offset
+.endm
 
-                                   .macro NOT_SIBLING_CALL_HINT 876
-    :.pushsection.discard.not_sibling_call.long 876b -
-                                   ..popsection.endm
+.macro NOT_SIBLING_CALL_HINT
+876:	.pushsection .discard.not_sibling_call
+	.long 876b - .
+	.popsection
+.endm
 #endif /* !__ASSEMBLY__ */
 
 #endif /* end of include guard: UNWIND_HINTS_H_PJXUIKZC */
