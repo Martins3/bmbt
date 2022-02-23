439f6f7aba8a9aac111f555cd10395fb6e65560b
xueshi hu <hubachelar@gmail.com>
2022-02-23 16:41:42 +0800

    refactor: rename `duck_` functions to `kern_`

--- env/loongarch/syscall/internal.h
+++ env/loongarch/syscall/internal.h
@@ -1,12 +1,12 @@
 #ifndef INTERNAL_H_IMYZ2RLB
 #define INTERNAL_H_IMYZ2RLB
 
-int duck_printf(const char *fmt, ...);
+int kern_printf(const char *fmt, ...);
 
-void __duck_assert_fail(const char *expr, const char *file, int line,
+void __kern_assert_fail(const char *expr, const char *file, int line,
                         const char *func);
-#define duck_assert(x)                                                         \
-  ((void)((x) || (__duck_assert_fail(#x, __FILE__, __LINE__, __func__), 0)))
+#define kern_assert(x)                                                         \
+  ((void)((x) || (__kern_assert_fail(#x, __FILE__, __LINE__, __func__), 0)))
 
 long kernel_writev(long arg0, long arg1, long arg2, long arg3, long arg4,
                    long arg5, long arg6);
--- env/loongarch/syscall/writev.c
+++ env/loongarch/syscall/writev.c
@@ -8,8 +8,8 @@ long kernel_writev(long arg0, long arg1, long arg2, long arg3, long arg4,
   int iovcnt = (int)arg2;
   long ret = 0;
   // the only caller is __stdio_write
-  duck_assert(fd == 2);
-  duck_assert(iovcnt == 2);
+  kern_assert(fd == 2);
+  kern_assert(iovcnt == 2);
   for (int i = 0; i < 2; ++i) {
     struct iovec *io = iov + i;
     early_console_write(io->iov_base, io->iov_len);
