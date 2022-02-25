18edae69ac091dde71279bf3c31d5597c726b5ca
xueshi hu <hubachelar@gmail.com>
2022-02-25 14:27:34 +0800

    refactor(LATX): rename file

---
+++ script/rename-latx.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+dir=/home/maritns3/core/5000/core/bmbt/src/i386/LATX
+
+shopt -s globstar
+for file in "${dir}"/**; do
+  if [[ -f $file ]]; then
+    new_name="${file//_/-}"
+    if [[ $file != "$new_name" ]];then
+      mv "$file" "$new_name"
+    fi
+  fi
+done
+shopt -u globstar
