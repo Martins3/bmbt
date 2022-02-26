f3128e5ef409cbd247fce439695fa9851fd8234f
xueshi hu <hubachelar@gmail.com>
2022-02-26 20:56:28 +0800

    feat(LATX): add sys-sigint stub

---
+++ src/latx-stub.c
@@ -0,0 +1,5 @@
+#include "i386/LATX/include/latx-types.h"
+#include "i386/LATX/include/common.h"
+#include "i386/LATX/include/la-ir2.h"
+int sigint_enabled(void) { return 0; }
+void latxs_sigint_prepare_check_jmp_glue_2(IR2_OPND lst, IR2_OPND led) {}
