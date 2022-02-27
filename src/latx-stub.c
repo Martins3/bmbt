bea8b528d42f49b778b60981c8f0cc867109795e
xueshi hu <hubachelar@gmail.com>
2022-02-27 10:43:37 +0800

    fix(LATX): add more definitions to latx-stub.c

--- src/latx-stub.c
+++ src/latx-stub.c
@@ -1,5 +1,7 @@
-#include "i386/LATX/include/latx-types.h"
 #include "i386/LATX/include/common.h"
+#include "i386/LATX/include/latx-types.h"
 #include "i386/LATX/include/la-ir2.h"
+#include <exec/exec-all.h>
 int sigint_enabled(void) { return 0; }
 void latxs_sigint_prepare_check_jmp_glue_2(IR2_OPND lst, IR2_OPND led) {}
+void latxs_tb_relink(TranslationBlock *utb) {}
