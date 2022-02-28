b65289eac4c45989ea917800badd072178a977db
xueshi hu <hubachelar@gmail.com>
2022-02-28 10:37:58 +0800

    feat(LATX): add flags to run_xqm.sh to run xqm and latx
    below are verified:
    - xqm can't run hello world
    - softfloat_init are called

--- run_xqm.sh
+++ run_xqm.sh
@@ -1,4 +1,32 @@
 #!/bin/bash
+LATX=false
+
+function usage() {
+  echo "Usage :   [options] [--]
+
+  Options:
+  -h|help       Display this message
+  -x            RUN xqm
+  -l            Run latx"
+}
+
+while getopts "hxl" opt; do
+  case $opt in
+  h)
+    usage
+    exit 0
+    ;;
+  x) LATX=false ;;
+  l) LATX=true ;;
+  *)
+    echo -e "\n  Option does not exist : OPTARG\n"
+    usage
+    exit 1
+    ;;
+  esac # --- end of case ---
+done
+shift $((OPTIND - 1))
+
 BASE_DIR=$(pwd)
 BASE_DIR=/home/loongson/core/bmbt
 
@@ -7,25 +35,27 @@ kernel=~/xqm_images/vmlinuz-2.6.32
 
 kernel=${BASE_DIR}/image/bzImage.bin
 initrd=${BASE_DIR}/image/initrd.bin
-qemu=~/core/lat/build/i386-softmmu/qemu-system-i386
+if [[ $LATX = true ]]; then
+  qemu=~/core/lat/build/i386-softmmu/qemu-system-i386
+  arg_xqm="-latx select,cpjl=on,intblink=on,njc=on,largecc=on -accel tcg,tb-size=2048"
+else
+  qemu=~/ld/x86-qemu-mips/build/i386-softmmu/qemu-system-i386
+  arg_xqm="-xtm select,tblink,lbt,verbose"
+fi
 bios=${BASE_DIR}/seabios/out/bios.bin
 
-
 arg_bios="-chardev file,path=/tmp/seabios.log,id=seabios -device isa-debugcon,iobase=0x402,chardev=seabios -bios ${bios}"
-arg_xqm="-latx select,cpjl=on,intblink=on,njc=on,largecc=on -accel tcg,tb-size=2048"
 arg_img="-hda ~/xqm_images/ubuntu10s.test.img.full"
 arg_kernel="-kernel ${kernel}"
 arg_kernel_cmdline="-append \"console=ttyS0 \""
 arg_initrd=""
 
 if [[ $run_tiny_kernel == true ]]; then
-	arg_kernel_cmdline="-append \"console=ttyS0 earlyprintk=serial root=/dev/ram rdinit=/hello.out\""
-	arg_initrd="-initrd ${initrd}"
-	arg_img=""
+  arg_kernel_cmdline="-append \"console=ttyS0 earlyprintk=serial root=/dev/ram rdinit=/hello.out\""
+  arg_initrd="-initrd ${initrd}"
+  arg_img=""
 fi
 
 cmd="${qemu} ${arg_img} ${arg_kernel} ${arg_xqm} ${arg_bios} ${arg_kernel_cmdline} ${arg_initrd} --nographic"
 echo "${cmd}"
 eval "${cmd}"
-
-# root=/dev/sda1 ro init=/bin/bash tsc=reliable rw
