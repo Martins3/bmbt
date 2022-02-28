24f3e85fcef35973c122889d3b28374ca28868af
xueshi hu <hubachelar@gmail.com>
2022-02-28 15:44:49 +0800

    feat(LATX): sync qemu option

--- src/tcg/tcg-all.c
+++ src/tcg/tcg-all.c
@@ -28,8 +28,8 @@ void tcg_handle_interrupt(CPUState *cpu, int mask) {
 }
 
 int tcg_init() {
-  // default tcg_tb_size equals 0
-  bmbt_check(tcg_tb_size == 0);
+  // -accel tcg,tb-size=2048
+  tcg_tb_size = 2048;
   tcg_exec_init(tcg_tb_size * 1024 * 1024);
   return 0;
 }
