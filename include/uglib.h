d0b6f08992f7a12b1074923c8c37d1f48acd4638
xueshi hu <hubachelar@gmail.com>
2022-02-28 15:52:04 +0800

    fix(LATX): uglib.h's relative path

--- include/uglib.h
+++ include/uglib.h
@@ -4,11 +4,11 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-#include "../../glib/gmacros.h"
-#include "../../glib/gmappedfile.h"
-#include "../../glib/gmem.h"
-#include "../../glib/gstring.h"
-#include "../../glib/gtestutils.h"
-#include "../../glib/gtree.h"
+#include "../glib/gmacros.h"
+#include "../glib/gmappedfile.h"
+#include "../glib/gmem.h"
+#include "../glib/gstring.h"
+#include "../glib/gtestutils.h"
+#include "../glib/gtree.h"
 
 #endif /* end of include guard: GLIB_STUB_H_78QYZX2K */
