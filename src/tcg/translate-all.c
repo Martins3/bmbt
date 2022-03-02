8d024576cde91e986ee672765e22492f7db37071
xueshi hu <hubachelar@gmail.com>
2022-03-02 12:49:17 +0800

    refactor: create target/i386/translate.c for it's functions(#301)

--- src/tcg/translate-all.c
+++ src/tcg/translate-all.c
@@ -292,15 +292,6 @@ static int encode_search(TranslationBlock *tb, uint8_t *block) {
 }
 #endif
 
-void restore_state_to_opc(CPUX86State *env, TranslationBlock *tb,
-                          target_ulong *data) {
-  int cc_op = data[1];
-  env->eip = data[0] - tb->cs_base;
-  if (cc_op != CC_OP_DYNAMIC) {
-    env->cc_op = cc_op;
-  }
-}
-
 /* The cpu state corresponding to 'searched_pc' is restored.
  * When reset_icount is true, current TB will be interrupted and
  * icount should be recalculated.
