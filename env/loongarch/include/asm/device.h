887e8997056123c4bca155cff170d8d77517ad44
xueshi hu <hubachelar@gmail.com>
2022-03-09 00:36:22 +0800

    fix: introduce readb and writeb(#7)

---
+++ env/loongarch/include/asm/device.h
@@ -0,0 +1,8 @@
+#ifndef DEVICE_H_V9602GXP
+#define DEVICE_H_V9602GXP
+#include <asm/addrspace.h>
+#include <asm/mach-la64/loongson.h>
+
+#define LS_ISA_SERIAL_IO_BASE (TO_UNCAC(LOONGSON_REG_BASE + 0x1e0))
+
+#endif /* end of include guard: DEVICE_H_V9602GXP */
