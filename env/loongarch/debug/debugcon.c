145a6446557739e3f703e744408622d4c1531882
xueshi hu <hubachelar@gmail.com>
2022-03-10 21:41:32 +0800

    feat: add debugcon

---
+++ env/loongarch/debug/debugcon.c
@@ -0,0 +1,23 @@
+#include <asm/cpu-features.h>
+#include <asm/device.h>
+#include <asm/io.h>
+
+static void debugcon_putchar(char c) { writeb(c, LS_ISA_SERIAL_IO_BASE); }
+
+void debugcon_puts(const char *s) {
+  int num = 0;
+
+  if (!cpu_has_hypervisor) {
+    abort();
+  }
+  while (*s) {
+    if (*s == '\n')
+      debugcon_putchar('\r');
+    debugcon_putchar(*s);
+    s++;
+    num++;
+    if (num > 1000) {
+      abort();
+    }
+  }
+}
