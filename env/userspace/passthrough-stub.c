26595e788437979fc23d80334b4f0590f2c3f6f0
xueshi hu <hubachelar@gmail.com>
2022-03-18 14:56:05 +0800

    fix: add passthrough userspace stub

--- env/userspace/passthrough-stub.c
+++ env/userspace/passthrough-stub.c
@@ -1,1 +1,8 @@
-void setup_serial_pass_through() {}
+#include <stdint.h>
+#include <uglib.h>
+void setup_serial_pass_through() { g_assert_not_reached(); }
+void pci_pass_through_write(uint32_t addr, uint32_t val, int l) {
+  g_assert_not_reached();
+}
+uint32_t pci_pass_through_read(uint32_t addr, int l) { g_assert_not_reached(); }
+void pci_pass_through_init() { g_assert_not_reached(); }
