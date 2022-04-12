ab56a3d9e709076c489fb32bdbc2d49c7de5c7cf
xueshi hu <hubachelar@gmail.com>
2022-04-13 01:02:42 +0800

    fix: Loongson uart has a different frequency

--- env/loongarch/include/asm/device.h
+++ env/loongarch/include/asm/device.h
@@ -30,4 +30,20 @@
 #define X86_MSI_ADDR_BASE_LO 0xfee0100c
 #define X86_MSI_ENTRY_DATA_FLAG 0x4100
 
+// TMP_TODO move to somewhere suitable
+// arch/x86/boot/early_serial_console.c
+#define DLAB 0x80
+
+#define TXR 0 /*  Transmit register (WRITE) */
+#define RXR 0 /*  Receive register  (READ)  */
+#define IER 1 /*  Interrupt Enable          */
+#define IIR 2 /*  Interrupt ID              */
+#define FCR 2 /*  FIFO control              */
+#define LCR 3 /*  Line control              */
+#define MCR 4 /*  Modem control             */
+#define LSR 5 /*  Line Status               */
+#define MSR 6 /*  Modem Status              */
+#define DLL 0 /*  Divisor Latch Low         */
+#define DLH 1 /*  Divisor latch High        */
+
 #endif /* end of include guard: DEVICE_H_V9602GXP */
