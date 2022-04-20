348066d231053e18afa7b162ce8e41174e4b3478
xueshi hu <hubachelar@gmail.com>
2022-04-20 20:20:07 +0800

    refactor: move `struct resource` to standalone file

---
+++ env/loongarch/include/linux/ioport.h
@@ -0,0 +1,11 @@
+#ifndef IOPORT_H_IEXNH5BU
+#define IOPORT_H_IEXNH5BU
+
+#include <linux/type.h>
+
+struct resource {
+  u64 start;
+  u64 end;
+};
+
+#endif /* end of include guard: IOPORT_H_IEXNH5BU */
