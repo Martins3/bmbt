79266a814409c64436bc1e09e37be76bffe09375
xueshi hu <hubachelar@gmail.com>
2022-06-27 14:02:59 +0800

    fix: rename PCI functions

--- env/userspace/passthrough-stub.c
+++ env/userspace/passthrough-stub.c
@@ -8,10 +8,10 @@
 #include <uglib.h>
 
 void setup_serial_pass_through() { g_assert_not_reached(); }
-void pci_pass_through_write(uint32_t addr, uint32_t val, int l) {
+void pci_config_pass_write(uint32_t addr, uint32_t val, int l) {
   g_assert_not_reached();
 }
-uint32_t pci_pass_through_read(uint32_t addr, int l) { g_assert_not_reached(); }
+uint32_t pci_config_pass_read(uint32_t addr, int l) { g_assert_not_reached(); }
 void pci_pass_through_init() { g_assert_not_reached(); }
 void cpu_wait(void) {}
 
